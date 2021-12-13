d3435e1977804838e741326af22c87a78f17f984
Alex Wecker <wecal508@school.lu>
2021-12-13 15:09:00 +0100

    Created Timeline css file with style of the Timeline header image

--- resources/views/timelineOverview.blade.php
+++ resources/views/timelineOverview.blade.php
@@ -1,7 +1,25 @@
-<div>
-    Category is {{$id}}.
-    <?php
-        if (isset($eventId))
-        echo "Event is {$eventId}."
-    ?>
-</div>
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="{{ URL('css/timelineOverview.css') }}">
+    <title>Timeline</title>
+</head>
+<body>
+    <header style="background-image: url({{url('images/intro_page/Image7_modif@2x.png')}})">
+        <h1>{{$id}}</h1>
+    </header>
+{{--//    if (isset($eventId))--}}
+{{--//        echo "<h1>Event is {$eventId}</h1>";--}}
+{{--//    if (isset($languageid))--}}
+{{--//        echo "<h1>Language is {$languageid}</h1>";--}}
+{{--//    $texts = DB::table('CategoryLang')->get();--}}
+{{--//--}}
+{{--//    foreach ($texts as $text) {--}}
+{{--//        echo $text->dtText;--}}
+{{--//        echo "<br>";--}}
+{{--//    }--}}
+
+
+</body>
+</html>
+
