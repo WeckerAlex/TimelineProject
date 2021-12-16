4d13209c082b23e5920bad205f405039c1cc36ff
Samuel Ribeiro Soares <RibSa683@school.lu>
2021-12-16 13:00:26 +0100

    Intro page css with animation

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -3,27 +3,52 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
     <style>
         * { padding: 0; margin: 0; }
         body, html {
             background-color: rgba(181, 21, 21, 1);
-            height: 100vh;
-            display: table;
+            height: 90vh;
+            width: 80vw;
         }
 
         #categoryList {
-            color: white;
-            list-style-type: none;
-            height: 100%;
-            font-weight: bold;
-            display: table;
+            display: flex;
+            position: relative;
+            align-items: stretch;
+            flex-direction: column;
+            justify-content: space-between;
+            height: 92vh;
+            width: 90vw;
+            margin-top: 4vh;
+            margin-bottom: 4vh;
         }
 
-        ul li {
-            font-size: 70px;
-            display: table-row;
-            font-style: normal;
-            font-family: monospace;
+        li , .whiteRectangle {
+            display: inline-block;
+            list-style-type: none;
+            color: white;
+            font-family: Roboto;
+            font-size: 450%;
+            font-weight: bolder;
+            transition: 0.3s;
+        }
+
+        .whiteRectangle {
+            background-color: white;
+            width: 12vh;
+            margin-left: 2vw;
+            margin-right: 1vw;
+            height: 5.2vh;
+        }
+
+        li:hover > .whiteRectangle{
+            width: 22vh;
+            background-color: #5F0000;
+        }
+
+        li:hover {
+            color: #5F0000;
         }
 
     </style>
@@ -35,9 +60,10 @@
 
     $categories = DB::table('CategoryLang')->get();
 foreach ($categories as $category) {
-    echo "<li>";
+    $test = $category->dtText;
+    echo "<a href='Timeline/$test/fr'><li><div class='whiteRectangle'></div>";
     echo $category->dtText;
-    echo "</li>";
+    echo "</li></a>";
 }
 ?>
 </ul>
