60d36d804321ffeff8dc72547a7bb7c83412393e
Samuel Ribeiro Soares <RibSa683@school.lu>
2022-01-12 14:45:41 +0000

    Mobile css added / Splash screen added / LTAM logo added / minor css fixes

--- resources/views/timeline.blade.php
+++ resources/views/timeline.blade.php
@@ -1,4 +1,12 @@
-<!DOCTYPE html>
+<?php
+$cookie_name = "playSplash";
+$cookie_value = "No";
+
+if (!isset($_COOKIE[$cookie_name])) {
+    setcookie($cookie_name, $cookie_value, time() + (86400 * 1 / 8), "/"); // 86400 = 1 day
+}
+?>
+        <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -7,15 +15,21 @@
     <link rel="stylesheet" type="text/css" href="{{ URL('css/introduction.css') }}">
 <!--<link rel="stylesheet" type="text/css" href="{{ URL('css/colors.css') }}">-->
     <title>LAM125 - Introduction</title>
+    <link rel="shortcut icon" href="../../images/LTAM-logo.png">
 </head>
 <body>
 
-{{--<div class="splashScreen">--}}
-{{--    <img id="test" src='../../images/intro/Image3_modif.png'>--}}
-{{--    <img id="test2" src='../../images/intro/Image1_modif.png'>--}}
-{{--    <span id="firstTextAnimation">LAM EN CHIFFRE</span>--}}
-{{--    <span id="secondTextAnimation">FUTUR</span>--}}
-{{--</div>--}}
+@if(!isset($_COOKIE["playSplash"]))
+    <div id="splashScreen">
+        <!--<span id="skip" onclick="removeSplash()">skip</span>-->
+        <img id="topLeftImage" src='../../images/intro/Image3_modif.png'>
+        <img id="topRightImage" src='../../images/intro/Image1_modif.png'>
+        <img id="bottomLeftImage" src='../../images/intro/Image5_modif.png'>
+        <img id="bottomRightImage" src='../../images/intro/Image2_modif.png'>
+        <span id="firstTextAnimation">LAM EN CHIFFRE</span>
+        <span id="secondTextAnimation">FUTUR</span>
+    </div>
+@endif
 
 <ul id="categoryList">
     <?php
@@ -24,6 +38,8 @@
     $languages = DB::table('Language')->get();
     $counter = 1;
     $categoryArray = array();
+    $imageArray = array();
+    $lastItem = "";
 
     echo "<div id='languageChoice'>";
 
@@ -39,7 +55,7 @@
         }
 
         if ($counter != $amountLanguages)
-            echo " / ";
+            echo " <span id='slash'>/</span> ";
 
         $counter++;
 
@@ -67,7 +83,7 @@
         foreach ($french as $frenchText) {
             $categoryText = $frenchText->dtText;
         }
-        
+
         $removeSpaces = str_replace(' ', '_', $categoryText);
         $url = route('Timeline', ['category' => $removeSpaces, 'languageid' => $languageid]);
         echo "<a href=$url><li><div class='whiteRectangle'></div>";
@@ -76,7 +92,18 @@
         $categoryImage = DB::table('Media')->where('idMedia', DB::table('Category')->where('idCategory', $category->fiCategory)->value('fiMedia'))->get();
 
         foreach ($categoryImage as $image) {
-            echo "<img class='draftImg' src='../../images/home/$image->dtPath'></li></a>";
+
+            $categoriesLast = DB::table('CategoryLang')->where('fiLanguage', $langId)->orderby('fiCategory','DESC')->first("dtText");
+
+            foreach ($categoriesLast as $last) {
+                $lastItem = $last;
+            }
+
+            if ($lastItem == $category->dtText)
+                echo "<img class='draftImg' src='../../images/home/$image->dtPath'></li></a>";
+            else
+                echo "</li><img class='draftImg' src='../../images/home/$image->dtPath'></a>";
+            array_push($imageArray, $image->dtPath);
         }
 
     }
@@ -84,44 +111,148 @@
     ?>
     <script>
 
+        let firstInter = setInterval(swapText, 7000);
+
+        let splashScreen = document.getElementById("splashScreen");
         let firstText = document.getElementById("firstTextAnimation");
         let secondText = document.getElementById("secondTextAnimation");
+        let topLeftImage = document.getElementById("topLeftImage");
+        let topRightImage = document.getElementById("topRightImage");
+        let bottomLeftImage = document.getElementById("bottomLeftImage");
+        let bottomRightImage = document.getElementById("bottomRightImage");
+        let skip = document.getElementById("skip");
         let categoryArray = <?php echo json_encode($categoryArray);?>;
+        let imageArray = <?php echo json_encode($imageArray);?>;
         let counter = -2;
         let secondCounter = -1;
+        let flag = 0;
+        let imageCounter = 0;
 
+
+        function removeSplash() {
+            document.getElementById("splashScreen").style.display = "none";
+        }
 
         function swapText() {
 
-            if (counter + 2 < (categoryArray.length)) {
-                counter = counter + 2;
-                firstText.textContent = categoryArray[counter];
-            } else {
-                counter = counter + 2 - categoryArray.length;
-                firstText.textContent = categoryArray[counter];
-            }
+            if(firstText != null) {
 
+                if (counter + 2 < (categoryArray.length)) {
+                    counter = counter + 2;
+
+                    if (flag == 1) {
+                        clearInterval(firstInter);
+                        document.getElementById("splashScreen").style.display = "none";
+                    }
+
+                    if (categoryArray[categoryArray.length - 1] == categoryArray[counter])
+                        flag = 1;
+
+                    firstText.textContent = categoryArray[counter];
+
+                    if (imageCounter > imageArray.length - 1)
+                        imageCounter = 0;
+
+                    topLeftImage.src = "../../images/home/" + imageArray[imageCounter]
+
+                    imageCounter++;
+
+                    if (imageCounter > imageArray.length - 1)
+                        imageCounter = 0;
+
+                    topRightImage.src = "../../images/home/" + imageArray[imageCounter]
+
+                    imageCounter++;
+
+                    if (imageCounter > imageArray.length - 1)
+                        imageCounter = 0;
+
+                    bottomLeftImage.src = "../../images/home/" + imageArray[imageCounter]
+
+                    imageCounter++;
+
+                    if (imageCounter > imageArray.length - 1)
+                        imageCounter = 0;
+
+                    bottomRightImage.src = "../../images/home/" + imageArray[imageCounter]
+
+                    imageCounter++;
+
+                } else {
+                    if (flag == 1) {
+                        clearInterval(firstInter);
+                        document.getElementById("splashScreen").style.display = "none";
+                    }
+                }
+            }
+            else
+                clearInterval(firstInter);
         }
 
-        function swapSecondText() {
-
-            if (secondCounter + 2 < (categoryArray.length)) {
-                secondCounter = secondCounter + 2;
-                secondText.textContent = categoryArray[secondCounter];
-            } else {
-                secondCounter = secondCounter + 2 - categoryArray.length;
-                secondText.textContent = categoryArray[secondCounter];
-            }
-
-        }
-
-        setInterval(swapText, 8000)
         swapText();
 
         setTimeout(function () {
-            setInterval(swapSecondText, 8000)
+            globalThis.secondInter = setInterval(swapSecondText, 7000);
             swapSecondText();
-        }, 4000);
+        }, 3500);
+
+        function swapSecondText() {
+
+            if(secondText != null) {
+
+                if (secondCounter + 2 < (categoryArray.length)) {
+
+                    secondCounter = secondCounter + 2;
+
+                    if (flag == 1) {
+                        clearInterval(firstInter);
+                        document.getElementById("splashScreen").style.display = "none";
+                    }
+
+                    if (categoryArray[categoryArray.length - 1] == categoryArray[secondCounter])
+                        flag = 1;
+
+                    secondText.textContent = categoryArray[secondCounter];
+
+                    if (imageCounter > imageArray.length - 1)
+                        imageCounter = 0;
+
+                    topLeftImage.src = "../../images/home/" + imageArray[imageCounter]
+
+                    imageCounter++;
+
+                    if (imageCounter > imageArray.length - 1)
+                        imageCounter = 0;
+
+                    topRightImage.src = "../../images/home/" + imageArray[imageCounter]
+
+                    imageCounter++;
+
+                    if (imageCounter > imageArray.length - 1)
+                        imageCounter = 0;
+
+                    bottomLeftImage.src = "../../images/home/" + imageArray[imageCounter]
+
+                    imageCounter++;
+
+                    if (imageCounter > imageArray.length - 1)
+                        imageCounter = 0;
+
+                    bottomRightImage.src = "../../images/home/" + imageArray[imageCounter]
+
+                    imageCounter++;
+
+                } else {
+
+                    if (flag == 1) {
+                        clearInterval(firstInter);
+                        document.getElementById("splashScreen").style.display = "none";
+                    }
+                }
+            }
+            else
+                clearInterval(secondInter);
+        }
 
     </script>
 </ul>
